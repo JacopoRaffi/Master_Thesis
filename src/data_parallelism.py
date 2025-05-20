df1e634c69f4b6aea34be87c70245d4f8b0e6ff5
JacopoRaffi <raffijacopo@gmail.com>
2025-05-20 09:14:00 +0200

    fix ddp

--- src/data_parallelism.py
+++ src/data_parallelism.py
@@ -2,12 +2,19 @@ import argparse
 import torch
 from torch.nn.parallel import DistributedDataParallel as DDP
 import time
+import psutil
 import csv
 from utils import *
 
 torch.set_default_device("cpu")
+torch.manual_seed(42)
 
-#TODO: Add peak memory usage during training
+def get_memory_usage():
+    """Get the current memory usage of the process."""
+    process = psutil.Process()
+    mem = process.memory_info().rss / (1024 ** 2)  # Convert bytes to MB
+    return mem
+
 def synch_train(model:torch.nn, train_loader:torch.utils.data.DataLoader, val_loader:torch.utils.data.DataLoader, 
                 num_epochs:int, optimizer:torch.optim, train_loss:torch.nn, val_loss:callable, device:str='cpu', model_name:str='vit-base-patch16-224-in21k'):
     
@@ -20,9 +27,11 @@ def synch_train(model:torch.nn, train_loader:torch.utils.data.DataLoader, val_lo
     batch_size = train_loader.batch_size * world_size
     file_name = f"../log/rank_{rank}_synch_ddp_{world_size}_minibatch_{batch_size}_{model_name}_model.csv"
 
+    model = DDP(model)
+
     with open(file_name, mode="w+") as file:
         writer = csv.writer(file)
-        writer.writerow(["epoch", "batch_id", "loss", "forward_time", "backward_time", "phase"])
+        writer.writerow(["epoch", "batch_id", "loss", "forward_time", "backward_time", "peak_memory_usage(MB)", "phase"])
 
         for epoch in range(num_epochs):
             model.train()
@@ -35,6 +44,9 @@ def synch_train(model:torch.nn, train_loader:torch.utils.data.DataLoader, val_lo
                 outputs = model(images)
                 end_forward = time.time()
 
+                # Get the memory usage after forward pass
+                mem_usage_forward = get_memory_usage()
+
                 loss = train_loss(outputs.logits, labels)
 
                 start_backward = time.time()
@@ -44,7 +56,7 @@ def synch_train(model:torch.nn, train_loader:torch.utils.data.DataLoader, val_lo
                 optimizer.step()
 
                 # Log the stats
-                writer.writerow([epoch, i, loss.item(), end_forward - start_forward, end_backward - start_backward, "train"])
+                writer.writerow([epoch, i, loss.item(), end_forward - start_forward, end_backward - start_backward, mem_usage_forward, "train"])
                 file.flush()
 
             # Validation step
@@ -57,9 +69,12 @@ def synch_train(model:torch.nn, train_loader:torch.utils.data.DataLoader, val_lo
                     outputs = model(images)
                     end_forward = time.time()
 
+                    # Get the memory usage after forward pass
+                    mem_usage_forward = get_memory_usage()
+
                     accuracy = val_loss(outputs.logits, labels)
                     # Log the stats
-                    writer.writerow([epoch, i, accuracy, end_forward - start_forward, 0, "val"])
+                    writer.writerow([epoch, i, accuracy, end_forward - start_forward, 0, mem_usage_forward, "val"])
             
             file.flush()
 
@@ -74,13 +89,13 @@ if __name__ == "__main__":
                         help="Training mode: 'synch' for synchronous or 'asynch' for asynchronous")
     parser.add_argument("--model", type=str, default="google/vit-base-patch16-224-in21k",
                         help="Name of the ViT model to use")
-    parser.add_argument("--minibatch", type=int, default=5, 
+    parser.add_argument("--minibatch", type=int, default=512, 
                         help="Size of the minibatch for training")
     parser.add_argument("--num_epochs", type=int, default=1,
                         help="Number of epochs to train the model")
-    parser.add_argument("--lr", type=float, default=0.001,
+    parser.add_argument("--lr", type=float, default=1e-4,
                         help="Learning rate for the optimizer")
-    parser.add_argument("--weight_decay", type=float, default=0.0001,
+    parser.add_argument("--weight_decay", type=float, default=1e-5,
                         help="Weight decay for the optimizer")
     parser.add_argument("--asynch_iterations", type=int, default=10,
                         help="Number of iteration to be performed before synchronizing")
@@ -114,8 +129,10 @@ if __name__ == "__main__":
     loss = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.Adam(model.parameters(), lr=lr, weight_decay=weight_decay)
 
+    model_name = model_name.split("/")[-1] 
+
     if mode == "synch":
-        synch_train(model, train_loader, val_loader, num_epochs, optimizer, loss, accuracy)
+        synch_train(model, train_loader, val_loader, num_epochs, optimizer, loss, accuracy, model_name=model_name)
     else:
         asynch_train()
 
